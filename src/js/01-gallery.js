313e8fd22eac25d251ec35805d0017d08d31a9c3
MaksymilianBilski <biger9@o2.pl>
2022-10-02 15:15:38 +0200

    skopiowanie code z jshw07

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,4 +4,39 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+galleryItems.map(({ preview, original, description }) =>
+  gallery.insertAdjacentHTML(
+    'beforeend',
+    `<div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src=${preview}
+          data-source=${original}
+          alt=${description}
+        />
+      </a>
+    </div>`
+  )
+);
+
+const onClick = event => {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  console.log(event.target);
+  const instance = basicLightbox.create(
+    `<img src=${event.target.dataset.source} alt=${event.target.description}></img>`
+  );
+  instance.show();
+  const onEscape = key => {
+    if (key.code === 'Escape') {
+      instance.close();
+    }
+  };
+  window.addEventListener('keydown', onEscape);
+};
+
+gallery.addEventListener('click', onClick);
