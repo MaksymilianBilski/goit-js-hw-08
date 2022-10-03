1ed3ab10be987eb1ce7f7ecc8e849e24cca6cf20
MaksymilianBilski <biger9@o2.pl>
2022-10-03 19:07:22 +0200

    dfff

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Vimeo from '../../node_modules/@vimeo/player/dist/player.es';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
