4a0f84ef166da1e46e6b5fd0a313f84433c1fcc0
MaksymilianBilski <biger9@o2.pl>
2022-10-05 05:57:50 +0200

    zrobione 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -11,11 +11,40 @@ player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
 
-player
-  .getCurrentTime()
-  .then(function (seconds) {
-    // seconds = the current playback position
-  })
-  .catch(function (error) {
-    // an error occurred
-  });
+// przypisanie aktualnego czasu do zmiennej
+let currentTime = 0;
+
+player.on('timeupdate', function () {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      currentTime = seconds;
+      localStorage.setItem('current', currentTime);
+    })
+    .catch(function (error) {
+      error = 'some ting go wung';
+      return error;
+    });
+});
+localStorage.getItem('current', currentTime);
+
+window.addEventListener(
+  'load',
+  player
+    .setCurrentTime(localStorage.getItem('current', currentTime))
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    })
+);
+console.log(localStorage);
