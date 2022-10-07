7bf69105735d9da5fa088282fe0f79b6d877264b
MaksymilianBilski <biger9@o2.pl>
2022-10-07 20:24:28 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,36 @@
 const input = document.querySelector(`input[type="email"]`);
 const textarea = document.querySelector(`textarea[name="message"]`);
 const button = document.querySelector(`button[type="submit"]`);
+const form = document.querySelector('feedback-form');
+const obj = {
+  email: '',
+  textarea: '',
+};
 
 const callback1 = () => {
-  localStorage.setItem('feedback-form-state', { input: input.value });
+  obj.email = input.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(obj));
 };
 
 const callback2 = () => {
-  localStorage.setItem('feedback-form-state', { textarea: textarea.value });
+  obj.textarea = textarea.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(obj));
 };
+
 input.addEventListener('input', callback1);
 textarea.addEventListener('input', callback2);
 
-console.log(localStorage.getItem('textarea'));
-console.log(localStorage.getItem('input'));
+const check = () => {
+  const load = localStorage.getItem('feedback-form-state');
+  const parsedLoad = JSON.parse(load);
+  if (localStorage.getItem('feedback-form-state') !== null) {
+    input.value = parsedLoad.email;
+    textarea.value = parsedLoad.textarea;
+  }
+  // if (){}
+};
+window.addEventListener('load', check);
+
+console.log(localStorage.getItem('feedback-form-state'));
+console.log(localStorage);
+console.log(obj);
