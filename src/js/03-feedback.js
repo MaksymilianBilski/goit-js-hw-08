635ed0967b2ae7c86b6092145fce9cbb9d9bdcbf
MaksymilianBilski <biger9@o2.pl>
2022-10-07 05:53:00 +0200

    poczatek 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,16 @@
+const input = document.querySelector(`input[type="email"]`);
+const textarea = document.querySelector(`textarea[name="message"]`);
+const button = document.querySelector(`button[type="submit"]`);
 
+const callback1 = () => {
+  localStorage.setItem('feedback-form-state', { input: input.value });
+};
+
+const callback2 = () => {
+  localStorage.setItem('feedback-form-state', { textarea: textarea.value });
+};
+input.addEventListener('input', callback1);
+textarea.addEventListener('input', callback2);
+
+console.log(localStorage.getItem('textarea'));
+console.log(localStorage.getItem('input'));
