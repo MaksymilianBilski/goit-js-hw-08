1e599f4d604a198a45bceb9aeb2f7f081af9a97c
MaksymilianBilski <biger9@o2.pl>
2022-10-08 05:19:54 +0200

    do poprawek stylistycznych

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,22 @@
+const { clear } = require('i/lib/inflections');
+
+// html tags
 const input = document.querySelector(`input[type="email"]`);
 const textarea = document.querySelector(`textarea[name="message"]`);
 const button = document.querySelector(`button[type="submit"]`);
 const form = document.querySelector('feedback-form');
+// html tags
+
+//object and local storage
 const obj = {
   email: '',
   textarea: '',
 };
+const load = localStorage.getItem('feedback-form-state');
+const parsedLoad = JSON.parse(load);
+//object and local storage
 
+//callbacks and listeners for email and  for message
 const callback1 = () => {
   obj.email = input.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(obj));
@@ -16,21 +26,38 @@ const callback2 = () => {
   obj.textarea = textarea.value;
   localStorage.setItem('feedback-form-state', JSON.stringify(obj));
 };
-
 input.addEventListener('input', callback1);
 textarea.addEventListener('input', callback2);
+//callbacks and listeners for email and  for message
 
+//submit event
+const handleSubmit = evt => {
+  evt.preventDefault();
+  if (input.value === '' || textarea.value === '') {
+    return alert('please fill all the fields!');
+  } else if (localStorage.length === 0) {
+    return;
+  } else if (parsedLoad !== '') {
+    obj.email = input.value;
+    obj.textarea = textarea.value;
+    console.log(obj);
+    console.log(localStorage);
+    localStorage.clear();
+  }
+};
+button.addEventListener('click', handleSubmit);
+//submit event
+
+//reload page
 const check = () => {
-  const load = localStorage.getItem('feedback-form-state');
-  const parsedLoad = JSON.parse(load);
-  if (localStorage.getItem('feedback-form-state') !== null) {
+  if (localStorage === null) {
+    return;
+  } else if (localStorage.getItem('feedback-form-state') === null) {
+    return;
+  } else if (parsedLoad.email !== null || parsedLoad.textarea !== null) {
     input.value = parsedLoad.email;
     textarea.value = parsedLoad.textarea;
   }
-  // if (){}
 };
 window.addEventListener('load', check);
-
-console.log(localStorage.getItem('feedback-form-state'));
-console.log(localStorage);
-console.log(obj);
+//reload page
