3315b3650c661b4326535a1a256804873b58cc77
MaksymilianBilski <biger9@o2.pl>
2022-11-06 15:36:21 +0100

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,54 @@
+// import vimeo from '@vimeo/player';
+// import _ from '/node_modules/lodash.throttle/index.js';
+// var throttle = require('lodash.throttle');
+// const iframe = document.querySelector('iframe');
+// const player = new vimeo(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+// // przypisanie aktualnego czasu do zmiennej
+// let currentTime = 0;
+
+// const readStorageTime = () => {
+//   player
+//     .getCurrentTime()
+//     .then(function (seconds) {
+//       localStorage.setItem('videoplayer-current-time', currentTime);
+//       currentTime = seconds;
+//       console.log(currentTime);
+//     })
+//     .catch(function (error) {
+//       error = 'some ting go wung';
+//       return error;
+//     });
+// };
+
+// const playFromStorageTime = () => {
+//   player
+//     .setCurrentTime(
+//       localStorage.getItem('videoplayer-current-time', currentTime)
+//     )
+//     .then(function (seconds) {})
+//     .catch(function (error) {
+//       switch (error.name) {
+//         case 'RangeError':
+//           break;
+//         default:
+//           break;
+//       }
+//     });
+// };
+// window.addEventListener('load', playFromStorageTime);
+// player.on('timeupdate', throttle(readStorageTime, 1000));
+
 import vimeo from '@vimeo/player';
-import _ from '/node_modules/lodash.throttle/index.js';
-var throttle = require('lodash.throttle');
+import _ from 'lodash.throttle/index';
 const iframe = document.querySelector('iframe');
 const player = new vimeo(iframe);
 
@@ -11,38 +59,3 @@ player.on('play', function () {
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
-
-// przypisanie aktualnego czasu do zmiennej
-let currentTime = 0;
-
-const readStorageTime = () => {
-  player
-    .getCurrentTime()
-    .then(function (seconds) {
-      localStorage.setItem('videoplayer-current-time', currentTime);
-      currentTime = seconds;
-      console.log(currentTime);
-    })
-    .catch(function (error) {
-      error = 'some ting go wung';
-      return error;
-    });
-};
-
-const playFromStorageTime = () => {
-  player
-    .setCurrentTime(
-      localStorage.getItem('videoplayer-current-time', currentTime)
-    )
-    .then(function (seconds) {})
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          break;
-        default:
-          break;
-      }
-    });
-};
-window.addEventListener('load', playFromStorageTime);
-player.on('timeupdate', throttle(readStorageTime, 1000));
